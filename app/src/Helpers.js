1de850c3267da5c0d2feeebb4481813c91dfd055
Amir Honarpour <honarpour@gmail.com>
2018-02-21 00:41:28 -0500

    fix(helper): Fix urls

--- app/src/Helpers.js
+++ app/src/Helpers.js
@@ -3,13 +3,13 @@ import Config from './Config';
 export const getCtrlUrl = instanceId =>
   `${window.location
     .toString()
-    .replace(`${Config.secret}/${instanceId}/0`, '')
-    .replace(`${Config.secret}/${instanceId}/1`, '')}ctrl/${
+    .replace(`/${Config.secret}/${instanceId}/0`, '')
+    .replace(`/${Config.secret}/${instanceId}/1`, '')}/ctrl/${
     Config.secret
   }/${instanceId}`;
 
 export const getShareUrl = instanceId =>
   `${window.location
     .toString()
-    .replace(`${Config.secret}/${instanceId}/0`, '')
-    .replace(`${Config.secret}/${instanceId}/1`, '')}join/${instanceId}`;
+    .replace(`/${Config.secret}/${instanceId}/0`, '')
+    .replace(`/${Config.secret}/${instanceId}/1`, '')}/join/${instanceId}`;
